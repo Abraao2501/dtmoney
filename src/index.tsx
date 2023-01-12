d5e5808a030d67b53a6dc2acda1f368d17d7f337
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2023-01-12 15:49:40 -0300

    Instalando e configurando o MirageJs para fornecer os dados da aplicação

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('transactions',() => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amouth: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +30,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
