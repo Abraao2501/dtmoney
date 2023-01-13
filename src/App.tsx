6b39400db042dc19178c4f4d1713439110e205ea
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2023-01-13 15:16:07 -0300

    Instalando e conigurando o uso da biblioteca react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransitionModalOpen, setisNewTransition] = useState(false);
+  
+
+  function handleOpenNewTransitionModal(){
+    setisNewTransition(true);
+  }
+
+  function handleCloseNewTransitionModal(){
+    setisNewTransition(false);
+  }
+
   return ( 
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransitionModal} />
       <Dashboard />
+
+      <Modal 
+          isOpen={isNewTransitionModalOpen} 
+          onRequestClose={handleCloseNewTransitionModal}
+        >
+            <h2>Cadastrar Informações</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
