040b0fca2d4592ef7e5818169a1cc0528517f7f0
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2023-01-17 15:52:31 -0300

    Listando transsações e trabalhando com o método seed da api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,18 +1,30 @@
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 import { api } from '../../services/api'
 import { Container } from './styles'
 
+interface Transaction {
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createAt: string,
+
+}
+
 export function TransactionsTable(){
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
 
   useEffect(() =>{
     api.get('/transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
   },[])
 
   return (
     <Container>
       <table>
-        <thead>
+        <thead> 
           <tr>
             <th>Título</th>
             <th>Valor</th>
@@ -22,19 +34,16 @@ export function TransactionsTable(){
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className='deposit'>R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2022</td>
-          </tr>
-
-          <tr>
-            <td>Aluguel</td>
-            <td className='withdraw'>- R$1100,00</td>
-            <td>Casa</td>
-            <td>28/02/2022</td>
-          </tr>
+          {
+            transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createAt}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,30 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance De Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createAt: new Date('2023-01-17 10:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createAt: new Date('2023-01-10 14:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
     this.get('transactions',() => {
